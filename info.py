011ef17c0011fababdf19d6ff67f2e0fbb279689
ROCKIND <136447110+ROCKIND@users.noreply.github.com>
2025-04-29 22:22:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'media_search')
 API_ID = int(environ.get('API_ID', '25833520'))
 API_HASH = environ.get('API_HASH', '7d012a6cbfabc2d0436d7a09d8362af7')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7211101354:AAG45fRhhFof2G9mv_SBDEjogEdKHLtcZ1k")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6317806472:AAETBU5A75ArqvnJOz1v_Cm4RT41yhQApWQ")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
